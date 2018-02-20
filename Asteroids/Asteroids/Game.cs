540adfb307b7d1e5ce05474b857af553691acd1a
Andrievsky Alexey <andrievskyalexey@gmail.com>
2018-02-20 12:41:08 +0300

    BaseObject Class added Game class modified

--- Asteroids/Asteroids/Game.cs
+++ Asteroids/Asteroids/Game.cs
@@ -15,6 +15,14 @@ namespace Asteroids
         static Game()
         {
         }
+
+        public static BaseObject[] _objs;
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            for (int i = 0; i < _objs.Length; i++)
+                _objs[i] = new BaseObject(new Point(600, i * 20), new Point(15 - i, 15 - i), new Size(20, 20));
+        }
         public static void Init(Form form)
         {
             // Графическое устройство для вывода графики
@@ -28,14 +36,34 @@ namespace Asteroids
             // Связываем буфер в памяти с графическим объектом.
             // для того, чтобы рисовать в буфере
             Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Load();
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
         }
+
         public static void Draw()
         {
             // Проверяем вывод графики
+            //Buffer.Graphics.Clear(Color.Black);
+            //Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            //Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            //Buffer.Render();
             Buffer.Graphics.Clear(Color.Black);
-            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
-            Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
             Buffer.Render();
         }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
     }
 }
