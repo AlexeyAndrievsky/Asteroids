8f395f5aefbe104bae19e41a3fcbb8d02d43ab78
Andrievsky Alexey <andrievskyalexey@gmail.com>
2018-02-20 19:43:49 +0300

    Files Added

---
+++ Asteroids/Asteroids/GameObj.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    [Serializable]
+    class GameObj
+    {
+        public string imgName;
+        public string resType;
+
+        public GameObj()
+        {
+
+        }
+
+        public GameObj(string imgName, string resType)
+        {
+            this.imgName = imgName;
+            this.resType = resType;
+        }
+    }
+}
---
+++ Asteroids/Asteroids/XmlLoader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Asteroids
+{
+    class XmlLoader
+    {
+        string fileName;
+        List<GameObj> ResList;
+
+        public string FileName
+        {
+            set { fileName = value; }
+        }
+
+        public int Count
+        {
+            get { return ResList.Count; }
+        }
+
+        public XmlLoader(string fileName)
+        {
+            this.fileName = fileName;
+            ResList = new List<GameObj>();
+        }
+
+        public void Load()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<GameObj>));
+            Stream fStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            ResList = (List<GameObj>)xmlFormat.Deserialize(fStream);
+            fStream.Close();
+        }
+    }
+}
